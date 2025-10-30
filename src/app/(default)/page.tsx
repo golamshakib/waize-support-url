9fcb3cce2c2a15721366b85e87890a28ed6bb8f3
golamshakib <golamshakib.h@gmail.com>
2025-10-30 19:45:29 +0600

    solve all problem and remove route from page.tsx

--- src/app/(default)/page.tsx
+++ src/app/(default)/page.tsx
@@ -1,19 +1,12 @@
 import ContactUs from "../components/ContactUs";
-import DeleteResult from "../components/DeleteResult";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import Instruction from "../components/Instruction";
-import Security from "../components/Security";
 
 export default function page() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-slate-950 via-slate-900 to-slate-950 text-white">
       <div className="max-w-4xl mx-auto px-6 py-12 md:py-20">
         <Header />
-        <Instruction />
-        <DeleteResult />
-        <Security />
-        {/* <UserGeneratedContent /> */}
         <ContactUs />
         <Footer />
       </div>
